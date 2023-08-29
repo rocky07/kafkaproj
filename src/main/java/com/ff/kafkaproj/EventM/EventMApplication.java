b94a0c0d8a8216fb3a583497ba1f02118140c25a
rakesh ramesan <ramesan.rakesh@gmail.com>
2023-08-29 03:01:08 +0000

    libs added to pom

---
+++ src/main/java/com/ff/kafkaproj/EventM/EventMApplication.java
@@ -0,0 +1,13 @@
+package com.ff.kafkaproj.EventM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMApplication.class, args);
+	}
+
+}
